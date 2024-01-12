9c19f03a933451902261b7f1bdcbf594b963c7fe
adryannferreiraa <154082695+adryannferreiraa@users.noreply.github.com>
2024-01-12 12:06:31 -0300

    d

--- scripts.js
+++ scripts.js
@@ -1,45 +1,38 @@
-// Simulação de dados para postagens
-const postsData = [
-    { user: "Nome1", content: "Conteúdo da postagem 1" },
-    { user: "Nome2", content: "Conteúdo da postagem 2" },
-    // Adicione mais postagens conforme necessário
-];
+let currentSlide = 0;
 
-// Função para exibir postagens na seção de postagens
-function exibirPostagens() {
-    const postagensSection = document.getElementById("postagens");
+function showSlide(index) {
+    const slides = document.querySelectorAll('.featured-news article');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
 
-    postsData.forEach(post => {
-        const postElement = document.createElement("div");
-        postElement.classList.add("post");
-
-        const userInfo = document.createElement("p");
-        userInfo.textContent = post.user;
-        postElement.appendChild(userInfo);
-
-        const postContent = document.createElement("p");
-        postContent.textContent = post.content;
-        postElement.appendChild(postContent);
-
-        postagensSection.appendChild(postElement);
+    slides.forEach((slide, i) => {
+        slide.style.display = i === currentSlide ? 'block' : 'none';
     });
 }
 
-// Simulação de dados para amigos sugeridos
-const amigosSugestoesData = ["Amigo1", "Amigo2", "Amigo3"];
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
 
-// Função para exibir amigos sugeridos na barra lateral
-function exibirAmigosSugestoes() {
-    const amigosSugestoesSection = document.getElementById("amigos-sugestoes");
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
 
-    amigosSugestoesData.forEach(amigo => {
-        const amigoElement = document.createElement("div");
-        amigoElement.classList.add("amigo");
+function searchNews() {
+    const searchTerm = document.getElementById('searchInput').value.toLowerCase();
+    const articles = document.querySelectorAll('.latest-news article');
 
-        const amigoNome = document.createElement("p");
-        amigoNome.textContent = amigo;
-        amigoElement.appendChild(amigoNome);
-
-        amigosSugestoesSection.appendChild(amigoElement);
+    articles.forEach(article => {
+        const title = article.querySelector('h3').innerText.toLowerCase();
+        if (title.includes(searchTerm)) {
+            article.style.display = 'block';
+        } else {
+            article.style.display = 'none';
+        }
     });
 }
